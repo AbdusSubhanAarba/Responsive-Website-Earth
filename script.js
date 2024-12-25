f1e158855ee41c0115ba9a66a47f7320315a92e9
Abdus Subhan Aarba <abdusubhan7@gmail.com>
2024-12-26 02:12:07 +0300

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// Select the menu icon and nav links
 const menuIcon = document.getElementById('menu-icon');
-const navLinks = document.querySelector('nav ul'); // Updated to match the real structure
-
-// Toggle the 'show' class on the nav-links when the menu icon is clicked
+const navLinks = document.querySelector('nav ul'); 
 menuIcon.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
